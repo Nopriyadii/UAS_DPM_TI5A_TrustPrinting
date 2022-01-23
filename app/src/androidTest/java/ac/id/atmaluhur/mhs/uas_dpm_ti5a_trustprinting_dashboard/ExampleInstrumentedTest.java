e6ef6db897675dd2c97b99e79a05891bc67e20df
Nopriyadii <1911500144@mahasiswa.atmaluhur.ac.id>
2022-01-23 03:10:15 -0800

    upload

--- app/src/androidTest/java/ac/id/atmaluhur/mhs/uas_dpm_ti5a_trustprinting_dashboard/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ac/id/atmaluhur/mhs/uas_dpm_ti5a_trustprinting_dashboard/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package ac.id.atmaluhur.mhs.trustPrinting;
+package ac.id.atmaluhur.mhs.uas_dpm_ti5a_trustprinting_dashboard;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ac.id.atmaluhur.mhs.trustPrinting", appContext.getPackageName());
+        assertEquals("ac.id.atmaluhur.mhs.uas_dpm_ti5a_trustprinting_dashboard", appContext.getPackageName());
     }
 }
--- app/src/test/java/ac/id/atmaluhur/mhs/uas_dpm_ti5a_trustprinting_dashboard/ExampleUnitTest.java
+++ app/src/test/java/ac/id/atmaluhur/mhs/uas_dpm_ti5a_trustprinting_dashboard/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package ac.id.atmaluhur.mhs.trustPrinting;
+package ac.id.atmaluhur.mhs.uas_dpm_ti5a_trustprinting_dashboard;
 
 import org.junit.Test;
 
