2f599efd3a55584316468c4afd296c5c97a077f9
megipranataa <megipranata01@gmail.com>
2022-01-23 17:55:56 +0700

    upload

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/uas_dpm_ti5a_trustprinting_login/MainActivity.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.mhs.uas_dpm_ti5a_trustprinting_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
